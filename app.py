81cf10fb027334e5c19791c8ae99567ee75e8785
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-12-07 16:19:53 +0200

    adding home page styling

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from website import socketio, create_app
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
     
